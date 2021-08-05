f313ebcbdad7a7a200a7be7f282a6258a447bedc
Anna Suuronen <annasuuronen@icloud.com>
2021-08-06 01:48:44 +0300

    Rekisteröinnin lisäys

--- functions.py
+++ functions.py
@@ -6,3 +6,11 @@ def get_password(username):
     password = result.fetchone()
     return password
 
+def sign_up(username, hash_value);
+    try:
+        sql = "insert into users (username, password, admin) values (:username, :password, False)"
+        db.session.execute(sql, {"username":username, "password":hash_value})
+        db.session.commit()
+        return True
+    except:
+        return False
--- routes.py
+++ routes.py
@@ -28,4 +28,22 @@ def sign_up():
 
 @app.route("/sign_up-action", methods=["POST"])
 def sign_up_action():
-    return "Kesken"
+    username = request.form["username"]
+    password = request.form["password"]
+    password_again = request.form["password_again"]
+    if username == "" or password == "":
+        return "Syötä käyttäjänimi ja salasana"
+    if len(username) > 15 or len(username) < 5:
+        return "Syötä käyttäjänimi, joka vastaa pituusvaatimuksia"
+    if len(password) > 18 or len(password) < 8:
+        return "Syötä salasana, joka vastaa pituusvatimuksia"
+    if password != password_again:
+        return "Salasanat eivät täsmää"
+    hash_value = generate_password_hash(password)
+    successful_sign_up = functions.sign_up(username, hash_value)
+    if successful_sign_up:
+        return redirect("/")
+    else:
+        return "Käyttäjätunnuksen luonti epäonnistui"
+
+
